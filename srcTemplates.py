90c4fc50d06ba8cd92d58a35d5c463e198064fc5
rmn_ubuntu <rmn@rmn.rmn>
2021-11-02 18:04:19 +0100

    first commit

---
+++ srcTemplates.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import os
+from sys import argv
+
+def python_script():
+    pass
+
+def python_program():
+    pass
+
+def c_program():
+    pass
+
+def cpp_program():
+    pass
+
+def cpp_class(classname: str):
+    pass
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
